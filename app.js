0b54bc0134be58f07fd942d4cecb91718918b651
cjgmj <cjgmj@hotmail.com>
2020-10-03 16:59:51 +0200

    Parámetros sin nombre

--- app.js
+++ app.js
@@ -45,10 +45,43 @@
 // saludar(fnTemp, persona);
 
 // **************** Valores por defecto arguments ****************
-function sumar(a = 1, b = 2) {
-    console.log(arguments);
-}
+// function sumar(a = 1, b = 2) {
+//     console.log(arguments);
+// }
 
 // Los valores por defecto no se ven reflejados en el arguments
-sumar();
-sumar(1, 2);
+// sumar();
+// sumar(1, 2);
+
+// **************** Parámetros sin nombre ****************
+// ES5
+// function agregar_alumno() {
+//     console.log(arguments);
+
+//     for (var i = 1; i < arguments.length; i++) {
+//         arguments[0].push(arguments[i]);
+//     }
+
+//     return arguments[0];
+// }
+
+// var alumnos_arr = ["John"];
+// var alumnos_arr2 = agregar_alumno(alumnos_arr, "Jane", "Linus", "Rasmus");
+
+// console.log(alumnos_arr2);
+
+// ES6
+function agregar_alumno(arr_alumnos, ...alumnos) {
+    console.log(arguments);
+
+    for (let i = 0; i < alumnos.length; i++) {
+        arr_alumnos.push(alumnos[i]);
+    }
+
+    return arr_alumnos;
+}
+
+let alumnos_arr = ["John"];
+let alumnos_arr2 = agregar_alumno(alumnos_arr, "Jane", "Linus", "Rasmus");
+
+console.log(alumnos_arr2);
