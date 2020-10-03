f1fa88e92d02f417f9cac4d594b333cca3c11d88
cjgmj <cjgmj@hotmail.com>
2020-10-03 16:49:40 +0200

    Valores por defecto arguments

--- app.js
+++ app.js
@@ -19,27 +19,36 @@
 // saludoES6();
 
 // function saludar(fn = function() { console.log("Hola Mundo"); }) {
-function saludar(fn = fnTemp, persona = {
-    nombre: "John"
-}) {
-    // console.log(typeof fn);
+// function saludar(fn = fnTemp, persona = {
+//     nombre: "John"
+// }) {
+//     // console.log(typeof fn);
 
-    if (typeof fn === "undefined") {
-        console.error("No es una función");
-        return;
-    }
+//     if (typeof fn === "undefined") {
+//         console.error("No es una función");
+//         return;
+//     }
 
-    fn();
-    console.log(persona);
+//     fn();
+//     console.log(persona);
+// }
+
+// function fnTemp() {
+//     console.log("Hola mundo FN");
+// }
+
+// saludar();
+
+// var persona = {
+//     nombre: "Jane"
+// };
+// saludar(fnTemp, persona);
+
+// **************** Valores por defecto arguments ****************
+function sumar(a = 1, b = 2) {
+    console.log(arguments);
 }
 
-function fnTemp() {
-    console.log("Hola mundo FN");
-}
-
-saludar();
-
-var persona = {
-    nombre: "Jane"
-};
-saludar(fnTemp, persona);
+// Los valores por defecto no se ven reflejados en el arguments
+sumar();
+sumar(1, 2);
