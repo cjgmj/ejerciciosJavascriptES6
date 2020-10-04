bcf5ed95d7029c8482bbb5c7286507df9d5e6897
cjgmj <cjgmj@hotmail.com>
2020-10-04 19:50:12 +0200

    Orden enumeración propiedades objetos

--- app.js
+++ app.js
@@ -105,21 +105,51 @@
 //     return objReceptor;
 // }
 
-var objReceptor = {};
+// var objReceptor = {};
 
-var objDonador = {
-    // nombre: "mi-archivo.js",
-    // Este get se introdujo en ES6 y devuelve el valor de la propiedad
-    // Es útil para procesar la propiedad antes de devolverla
-    // A la hora de mezclarlo se introduce como la propiedad y no como el get
-    get nombre() {
-        return "mi-archivo.js";
-    }
-};
+// var objDonador = {
+//     // nombre: "mi-archivo.js",
+//     // Este get se introdujo en ES6 y devuelve el valor de la propiedad
+//     // Es útil para procesar la propiedad antes de devolverla
+//     // A la hora de mezclarlo se introduce como la propiedad y no como el get
+//     get nombre() {
+//         return "mi-archivo.js";
+//     }
+// };
 
 // console.log(objDonador.nombre);
 // console.log(objDonador);
 // console.log(mezclar(objReceptor, objDonador));
 
 // ES6
-console.log(Object.assign(objReceptor, objDonador));
+// console.log(Object.assign(objReceptor, objDonador));
+
+// **************** Orden enumeración propiedades objetos ****************
+// Todas las llaves van en orden ascendente.
+// Todas las llaves tipo string van ordenadas en la manera que fueron agregadas
+// al objeto
+// Todos los símbolos van en el orden que fueron agregados al objeto.
+
+let objeto = {
+    c: 1,
+    0: 1,
+    x: 1,
+    15: 1,
+    r: 1,
+    3: 1,
+    b: 1
+};
+
+objeto.d = 1;
+objeto["2"] = 1;
+objeto["a"] = 1;
+
+console.log(Object.getOwnPropertyNames(objeto).join(","));
+
+console.log(Object.keys(objeto));
+
+console.log(JSON.stringify(objeto));
+
+for (let i in Object.keys(objeto)) {
+    console.log(Object.keys(objeto)[i]);
+}
