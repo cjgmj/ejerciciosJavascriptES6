c5349d9574fa846c4526dd75a88c0792e7ebb7e3
cjgmj <cjgmj@hotmail.com>
2020-10-04 19:29:06 +0200

    Nombre propiedades procesadas

--- app.js
+++ app.js
@@ -18,18 +18,57 @@
 // console.log(persona);
 
 // **************** Métodos concisos ****************
-var persona = {
-    nombre: "John",
+// var persona = {
+//     nombre: "John",
 
-    // ES5
-    // getNombre: function() {
-    //     console.log(this.nombre);
-    // }
+//     // ES5
+//     // getNombre: function() {
+//     //     console.log(this.nombre);
+//     // }
 
-    // ES6
-    getNombre() {
-        console.log(this.nombre);
-    }
+//     // ES6
+//     getNombre() {
+//         console.log(this.nombre);
+//     }
+// };
+
+// persona.getNombre();
+
+// **************** Nombre propiedades procesadas ****************
+// ES5
+// var persona = {};
+
+// var apellido = "apellido";
+
+// persona["nombre"] = "John";
+// persona[apellido] = "Doe";
+
+// console.log(persona["nombre"]);
+// console.log(persona[apellido]);
+
+// var persona = {
+//     "nombre": "Jane"
+// };
+
+// console.log(persona["nombre"]);
+
+// ES6
+// let apellido = "apellido";
+
+// let persona = {
+//     "nombre": "John",
+//     [apellido]: "Doe"
+// };
+
+// console.log(persona["nombre"]);
+// console.log(persona[apellido]);
+
+let suffix = "nombre";
+
+let persona = {
+    ["primer " + suffix]: "John",
+    ["segundo " + suffix]: "Howard"
 };
 
-persona.getNombre();
+console.log(persona["primer nombre"]);
+console.log(persona["segundo " + suffix]);
