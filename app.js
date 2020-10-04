b17ccacfc24a45da517bddc29e79aa7d725de239
cjgmj <cjgmj@hotmail.com>
2020-10-04 19:43:02 +0200

    Método Object.assing()

--- app.js
+++ app.js
@@ -74,23 +74,52 @@
 // console.log(persona["segundo " + suffix]);
 
 // **************** Método Object.is() ****************
-console.log(+0 == -0);
-console.log(+0 === -0);
-console.log(Object.is(+0, -0));
+// console.log(+0 == -0);
+// console.log(+0 === -0);
+// console.log(Object.is(+0, -0));
 
-console.log("*****");
+// console.log("*****");
 
-console.log(NaN == NaN);
-console.log(NaN === NaN);
-console.log(Object.is(NaN, NaN));
+// console.log(NaN == NaN);
+// console.log(NaN === NaN);
+// console.log(Object.is(NaN, NaN));
 
-console.log("*****");
+// console.log("*****");
 
-console.log(5 == 5);
-console.log(5 == "5");
+// console.log(5 == 5);
+// console.log(5 == "5");
 
-console.log(5 === 5);
-console.log(5 === "5");
+// console.log(5 === 5);
+// console.log(5 === "5");
 
-console.log(Object.is(5, 5));
-console.log(Object.is(5, "5"));
+// console.log(Object.is(5, 5));
+// console.log(Object.is(5, "5"));
+
+// **************** Método Object.assing() ****************
+// ES5
+// function mezclar(objReceptor, objDonador) {
+//     Object.keys(objDonador).forEach(function(key) {
+//         objReceptor[key] = objDonador[key];
+//     });
+
+//     return objReceptor;
+// }
+
+var objReceptor = {};
+
+var objDonador = {
+    // nombre: "mi-archivo.js",
+    // Este get se introdujo en ES6 y devuelve el valor de la propiedad
+    // Es útil para procesar la propiedad antes de devolverla
+    // A la hora de mezclarlo se introduce como la propiedad y no como el get
+    get nombre() {
+        return "mi-archivo.js";
+    }
+};
+
+// console.log(objDonador.nombre);
+// console.log(objDonador);
+// console.log(mezclar(objReceptor, objDonador));
+
+// ES6
+console.log(Object.assign(objReceptor, objDonador));
