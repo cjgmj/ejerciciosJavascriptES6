254ebbd66fc7c08475f9ebb9da3d87a342a89c87
cjgmj <cjgmj@hotmail.com>
2020-10-04 19:21:10 +0200

    Métodos concisos

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 // **************** Extensiones objetos literales ****************
-function crearPersona(nombre, apellido, edad) {
-    return {
-        // ES5
-        // nombre: nombre,
-        // apellido: apellido,
-        // edad: edad
+// function crearPersona(nombre, apellido, edad) {
+//     return {
+//         // ES5
+//         // nombre: nombre,
+//         // apellido: apellido,
+//         // edad: edad
 
-        // ES6
-        nombre,
-        apellido,
-        edad
-    };
-}
+//         // ES6
+//         nombre,
+//         apellido,
+//         edad
+//     };
+// }
 
-let persona = crearPersona("John", "Doe", 33);
+// let persona = crearPersona("John", "Doe", 33);
 
-console.log(persona);
+// console.log(persona);
+
+// **************** Métodos concisos ****************
+var persona = {
+    nombre: "John",
+
+    // ES5
+    // getNombre: function() {
+    //     console.log(this.nombre);
+    // }
+
+    // ES6
+    getNombre() {
+        console.log(this.nombre);
+    }
+};
+
+persona.getNombre();
