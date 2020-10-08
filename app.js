ad7d4aa7be99cb305031c16e4a9059d7a33ba75c
cjgmj <cjgmj@hotmail.com>
2020-10-08 19:47:07 +0200

    Clases como expresiones

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 // **************** Clases ****************
+// 1. Las clases funcionan muy parecido a la declaración LET.
+// 2. Todo el código dentro de la clase, funcione en modo estricto "strict mode".
+// 3. Todos los métodos no son enumerables.
+// 4. Todos los métodos internos no tienen un constructor.
+// 5. Llamar una clase sin el "new" dará un error.
+// 6. Intentar renombrar el nombre de la clase dentro de algún método de la misma
+// dará error.
+// 7. Tienen métodos estáticos y privados.
+
 // Clases en ES5
 // function Persona(nombre) {
 //     this.nombre = nombre;
@@ -26,25 +35,52 @@
 // console.log(persona2 instanceof Object);
 
 // **************** Clases en ES6 ****************
-class Persona {
-    constructor(nombre) {
-        this.nombre = nombre;
+// class Persona {
+//     constructor(nombre) {
+//         this.nombre = nombre;
+//     }
+
+//     // Esto es lo mismo que Persona.prototype.decirNombre...
+//     decirNombre() {
+//         console.log(this.nombre);
+//     }
+// }
+
+// let persona = new Persona("John");
+
+// persona.decirNombre();
+
+// // let persona2 = Persona("John"); // Devuelve un error porque es obligatorio usar new
+
+// console.log(persona instanceof Persona);
+// console.log(persona instanceof Object);
+
+// console.log(typeof Persona); // Esto devuelve function
+// console.log(typeof Persona.prototype.decirNombre);
+
+// **************** Clases como expresiones ****************
+// Se pueden crear de la misma forma que se crea una función
+// let miFuncion = function() {
+//     console.log("Hola mundo");
+// };
+
+// let otraFuncion = miFuncion;
+
+// console.log(typeof otraFuncion);
+
+// otraFuncion();
+
+let Persona = class {
+    constructor() {
+        this.nombre = "";
     }
 
-    // Esto es lo mismo que Persona.prototype.decirNombre...
     decirNombre() {
-        console.log(this.nombre);
+        console.log("Hola mundo");
     }
-}
+};
 
-let persona = new Persona("John");
+let persona = new Persona();
 
-persona.decirNombre();
-
-// let persona2 = Persona("John"); // Devuelve un error porque es obligatorio usar new
-
+console.log(typeof persona);
 console.log(persona instanceof Persona);
-console.log(persona instanceof Object);
-
-console.log(typeof Persona); // Esto devuelve function
-console.log(typeof Persona.prototype.decirNombre);
